00e2ac4e659f0f0ab29d8292e2b21429289bcd98
muazv-72 <muazv72@gmail.com>
2026-01-02 10:35:34 +0530

    Add files via upload

---
+++ traffic-light-main/app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,Response
+import cv2
+
+app=Flask(__name__)
+
+cap=cv2.VideoCapture(0)
+
+def generate():
+    while True:
+        ret,frame=cap.read()
+        if not ret:
+            break
+        else:
+            re,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+            yield(b'--frame\r\n'
+                  b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/video')
+def video():
+    return Response(generate(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__=="__main__":
+    app.run(port=8000,debug=True)
